9b6f5bdc59ea0dc81242dccca09aaee58152bd10
Adriano Camargo <adriano@grupoviabrasil.net.br>
2022-09-19 21:05:06 -0300

    Fim

---
+++ src/polimorfismo/Pato.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polimorfismo;
+
+/**
+ *
+ * @author adriano.camargo
+ */
+public class Pato extends Animal {
+
+    
+    public void comunicar() {
+        System.out.println("Quack quack");
+    }
+
+}
--- src/polimorfismo/Polimorfismo.java
+++ src/polimorfismo/Polimorfismo.java
@@ -15,7 +15,20 @@ public class Polimorfismo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        /*
+        Calculadora calc = new Calculadora();
+        System.out.println(calc.somar(2.00, 5.00));
+*/
+        
+        Gato gato = new Gato();
+        Cachorro cachorro = new Cachorro();
+        Pato pato = new Pato();
+        
+        gato.comunicar();
+        pato.comunicar();
+        cachorro.comunicar();
+        
+        
     }
     
 }
